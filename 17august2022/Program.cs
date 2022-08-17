d4d1b59012bbeba270845c87eb828c664033ef49
Nilofar Sheriff <nilofarsheriff@gmail.com>
2022-08-17 13:52:16 +0530

    Add project files.

---
+++ 17august2022/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17august2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string me;
+
+            Console.WriteLine("Enter a String");
+            me = Console.ReadLine();
+            Console.WriteLine("You wrote " + me);
+            Console.ReadLine();
+        }
+    }
+}
